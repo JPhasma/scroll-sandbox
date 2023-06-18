f7795d19e4139833d1c3d4c196012f825417575d
JaneDalton <26035724+JPhasma@users.noreply.github.com>
2023-06-18 17:29:54 +0100

    horizontal message update

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -4,8 +4,12 @@ function Hero({ subtitle }) {
   return (
     <div className='hero_header'>
       <h4>{subtitle}</h4>
-      <h1>Jane Doe</h1>
-      <h2>Fronted Dev (moving to Fullstack)</h2>
+      <h1>Jane's been busy...</h1>
+      <h2>This example could be used for the Recipe Carousel idea</h2>
+      <p>
+        I will be looking to work out POCs and examples for the other effects
+        and transitions ideas in next few weeks
+      </p>
     </div>
   );
 }
